d6bf971d67143061efe20da442eb7ccc79cbc716
bhush <bhush@DESKTOP-8H60880>
2022-07-02 18:38:09 +0530

    Add project files.

---
+++ a).dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart'; //package
+
+//widget tree is made below
+void main() {
+  //all the working and execution starts from here
+  runApp(
+    MaterialApp(
+      //widget (almost all the apps start from MaterialApp)
+      home: Scaffold(
+        appBar:AppBar(
+          title:Text("I am jacob Elordi");
+        )
+      )
+    ),
+  );
+}
